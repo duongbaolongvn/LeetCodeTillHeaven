bcbb144799a6d8b242fb08d41ff2824f40abc5cc
Jason Duong <77760726+duongbaolongvn@users.noreply.github.com>
2023-08-21 23:55:41 -0400

    Time: 125 ms (37.65%), Space: 16.2 MB (96.43%) - LeetHub

---
+++ 1763-longest-nice-substring/1763-longest-nice-substring.py
@@ -0,0 +1,8 @@
+class Solution:
+    def longestNiceSubstring(self, s: str) -> str:
+        ans = ""
+        for i in range(len(s)):
+            for ii in range(i+1, len(s)+1):
+                if all(s[k].swapcase() in s[i:ii] for k in range(i, ii)): 
+                    ans = max(ans, s[i:ii], key=len)
+        return ans 
